37a10399f1f7ed139bb8ca8bfa45a9c8cfbceb6c
Energy <71418770+energy-coresky@users.noreply.github.com>
2024-12-14 20:39:06 +0300

    Updated

--- diff.php
+++ diff.php
@@ -2,99 +2,101 @@
 
 class Diff
 {
-    static function parse($new, $old, $mode = false) {
-        $V = [];
-        $N = count($new = explode("\n", str_replace(["\r\n", "\r"], "\n", $new)));
-        $L = count($old = explode("\n", str_replace(["\r\n", "\r"], "\n", $old)));
+    static function diffx(string $new, string $old, $boundary = 3, $in = false) {
+        $ary = self::diff($new, $old, true, $boundary);
+        $out = $plus = '';
+        $add = $sub = $j = $last = 0;
+        $rest = $ary && is_int($last = $ary[0][0]) ? array_shift($ary) : false;
+        $len = $last > 999 ? 4 : 3;
+        $break = $last > 999 ? " =========\n" : " =======\n";
+        $in or $in = fn($x, $l, $n, $s) => ('=' == $x ? ' ' : ('+' == $x ? '+' : '-'))
+            . sprintf("%{$len}s|%{$len}s %s\n", $l, $n, $s);
+        if ($ary || $rest) {
+            foreach ($ary as $v) {
+                [$xn, $n, $new, $l, $old] = $v;
+                if ('+' == $xn || '*' == $xn) {
+                    $add++;
+                    $plus .= $in('+', $j = '', $n, $new);
+                }
+                if ('+' != $xn) {
+                    if ($eq = '=' == $xn) {
+                        if ($j && ++$j != $n && !$plus)
+                            $out .= $break;
+                        [$out, $plus, $j] = [$out . $plus, '', $n];
+                    } else {
+                        $sub++;
+                    }
+                    $out .= $in($xn, $l, $eq ? $n : '', $old);
+                }
+            }
+            $out .= $plus;
+            if ($rest) {
+                $cnt = count($ary = $rest[1] ?: $rest[2]);
+                ($plus = (bool)$rest[1]) ? ($add += $cnt) : ($sub += $cnt);
+                foreach ($ary as $i => $v)
+                    $out .= $plus ? $in('+', '', $last + $i, $v) : $in('-', $last + $i, '', $v);
+            }
+        }
+        return [$out, $add, $sub];
+    }
 
-        for ($n = $l = 0; $n < $N && $l < $L; ) {
-            $sn = current($new);
-            $sl = current($old);
+    static function diff(string $new, string $old, $mode = false, int $boundary = 3) {
+        $N = count($new = explode("\n", unl($new)));
+        $L = count($old = explode("\n", unl($old)));
+        $diff = $eq = [];
+        for ($rN = '', $n = $l = $rest = 0; $n < $N && $l < $L; ) {
+            $sn = pos($new);
+            $sl = pos($old);
             if ($sn === $sl) {
-                $V[$n++] = $l++;
+                if ($rest) {
+                    $rest--;
+                    $diff[] = ['=', ++$n, $sn, ++$l, $sl];
+                } else {
+                    $eq[] = ['=', ++$n, $sn, ++$l, $sl];
+                }
+                $rN .= '=';
                 array_shift($new);
                 array_shift($old);
             } else {
+                if ($eq)
+                    $diff = array_merge($diff, array_slice($eq, -$boundary));
+                $eq = [];
+                $rest = $boundary;
                 $fn = array_keys($old, $sn);
                 $fl = array_keys($new, $sl);
                 if (!$fn && !$fl) {
-                    $V[$n++] = '*';
-                    $l++;
+                    $rN .= '*';
+                    $diff[] = ['*', ++$n, $sn, ++$l, $sl];
                     array_shift($new);
-                    array_shift($old); # optimize?
+                    array_shift($old);
                 } elseif (!$fn) {
-                    $V[$n++] = '?';
+                    $rN .= '+';
+                    $diff[] = ['+', ++$n, $sn, 0, ''];
                     array_shift($new);
                 } elseif (!$fl) {
-                    $l++;
+                    $rN .= '.';
+                    $diff[] = ['.', 0, '', ++$l, $sl];
                     array_shift($old);
-                } else { # cross
-                    $tn = current($fn);
-                    $tl = current($fl);
-                    for ($in = 1; isset($new[$in]) && isset($old[$tn + $in]) && $new[$in] === $old[$tn + $in]; $in++);
-                    for ($il = 1; isset($old[$il]) && isset($new[$tl + $il]) && $old[$il] === $new[$tl + $il]; $il++);
-                    if ($in / $tn < $il / $tl) {
-                        $V[$n++] = '?';
+                } else { # both found
+                    $tn = pos($fn);
+                    $tl = pos($fl);
+                    for ($in = 1; ($new[$in] ?? 0) === ($old[$tn + $in] ?? 1); $in++);
+                    for ($il = 1; ($old[$il] ?? 0) === ($new[$tl + $il] ?? 1); $il++);
+                    if ($in / ++$tn / count($fn) < $il / ++$tl / count($fl)) {
+                        $rN .= '+';
+                        $diff[] = ['+', ++$n, $sn, 0, ''];
                         array_shift($new);
                     } else {
-                        $l++;
+                        $rN .= '.';
+                        $diff[] = ['.', 0, '', ++$l, $sl];
                         array_shift($old);
                     }
                 }
             }
         }
-        if ($mode)
-            return $V;
-
-        for ($n = $l = 0, $rN = '', $c = count($V); $n < $N || $l < $L; ) {
-            if ($n >= $N) {
-                $rN .= '.';
-                $l++;
-            } elseif ($l >= $L) {
-                $rN .= '+';
-                $n++;
-            } elseif ($n >= $c || $V[$n] === '*') {
-                $rN .= '*';
-                $n++;
-                $l++;
-            } elseif ($V[$n] === $l) {
-                $rN .= '=';
-                $n++;
-                $l++;
-            } elseif ($V[$n] !== '?') do {
-                    $rN .= '.';
-                } while ($V[$n] > ++$l);
-            else {
-                $cp = false;
-                for ($p = 1; $n + $p < $c; $p++)
-                    if (is_int($V[$n + $p])) {
-                        $cp = true;
-                        break;
-                    }
-                if ($cp) {
-                    $q = $V[$n + $p] - $l;
-                    while ($q && $p) {
-                        $rN .= "*";
-                        $n++;
-                        $l++;
-                        $q--;
-                        $p--;
-                    }
-                    while ($p--) {
-                        $rN .= "+";
-                        $n++;
-                    }
-                    while ($q--) {
-                        $rN .= ".";
-                        $l++;
-                    }
-                } else do {
-                    $rN .= '*';
-                    $n++;
-                    $l++;
-                } while ($n < $N && $l < $L);
-            }
-        }
-        return $rN;
+        $rest = max($N -= $n, $L -= $l);
+        if (!$mode)
+            return $rN . str_pad('', $rest, $N > $L ? '+' : '.');
+        return $rest ? array_merge([[$N > $L ? ++$n : ++$l, $new, $old]], $diff) : $diff;
     }
 }
